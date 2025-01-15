33616df3f58542ab25bf02db63d0fa3b2b3e6b0f
Stan Potemkin <stan@potemkin.su>
2025-01-15 09:35:15 +0300

    Make SPM Package

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "JMTimelineKit",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "JMTimelineKit",
+            targets: ["JMTimelineKit"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/DenTelezhkin/DTCollectionViewManager.git", from: "11.0.0"),
+        .package(url: "https://github.com/JivoChat/JMOnetimeCalculator.git", .branch("master")),
+        .package(url: "https://github.com/JivoChat/SwiftyNSException.git", .branch("main")),
+    ],
+    targets: [
+        .target(
+            name: "JMTimelineKit",
+            dependencies: [
+                .product(name: "DTCollectionViewManager", package: "DTCollectionViewManager"),
+                .product(name: "JMOnetimeCalculator", package: "JMOnetimeCalculator"),
+                .product(name: "SwiftyNSException", package: "SwiftyNSException"),
+            ],
+            path: "."
+        )
+    ]
+)
